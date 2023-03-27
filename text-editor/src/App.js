a681e7c013a75b95732b8086fc69adccfe789ce9
oriya aharoni <oriya5759@gmail.com>
2023-03-27 16:34:53 +0300

    hi3

--- text-editor/src/App.js
+++ text-editor/src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <h1>hi</h1>
         <h1>hi</h1>
+        <h1>hi</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
