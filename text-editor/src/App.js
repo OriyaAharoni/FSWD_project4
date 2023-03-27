f2d37fed97cf1b5065558a29420e30550837daa7
NoaShimshi <noanoashimshi@gmail.com>
2023-03-27 16:36:44 +0300

    delete hi

--- text-editor/src/App.js
+++ text-editor/src/App.js
@@ -6,8 +6,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>hi</h1>
-        <h1>hi</h1>
-        <h1>hi</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
